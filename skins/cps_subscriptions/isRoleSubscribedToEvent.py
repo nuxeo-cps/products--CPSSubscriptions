2c4c1a1bed4fdf0c78cf19433f1bc95a51f27413
janguenot <devnull@localhost>
2004-01-13 17:42:54 +0000

    Use to display checkbox

---
+++ skins/cps_subscriptions/isRoleSubscribedToEvent.py
@@ -0,0 +1,14 @@
+##parameters=event_type, role
+""" Is the role subscribed
+
+We gonna check if the role is subscribed in the context.
+"""
+
+portal_subscriptions = context.portal_subscriptions
+subscriptions = portal_subscriptions.getSubscriptionsFor(event_type, context, infos={})
+for subscription in subscriptions:
+    subscription_recipients_rule = subscription.getRecipientsRules()
+    for recipients_rule in subscription_recipients_rule:
+        if role in recipients_rule.getRoles():
+            return 1
+return 0
