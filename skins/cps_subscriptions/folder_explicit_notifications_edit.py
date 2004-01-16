86a887bf7d9b62ca9ad071bc20b54307d9533e96
janguenot <devnull@localhost>
2004-01-16 11:53:31 +0000

    Edit the ExplicitRecipientsRul for a given event

---
+++ skins/cps_subscriptions/folder_explicit_notifications_edit.py
@@ -0,0 +1,50 @@
+##parameters=REQUEST
+#$Id$
+""" Edit the explicit subscription configuration.
+
+It applies for a given event. The event_id is within the REQUEST.
+"""
+
+if REQUEST is not None:
+    if REQUEST.form:
+
+        #
+        # Checking if there's a local subscription container
+        #
+
+        subscription_id = context.portal_subscriptions.getSubscriptionId()
+        if subscription_id not in context.objectIds():
+            context.manage_addProduct[
+                'CPSSubscriptions'].addSubscriptionContainer()
+        subscription_folder = getattr(context, subscription_id)
+
+        #
+        # Checking the given request event
+        #
+
+        event_id = REQUEST.form.get('event_id', [])
+        if not hasattr(subscription_folder, event_id):
+            context.manage_addProduct[
+                'CPSSubscriptions'].addSubscription(id=evend_id)
+
+        #
+        # Checking the explicit recipients rules
+        #
+
+        event = getattr(subscription_folder, event_id)
+        explicit_recipients_rule_id = context.portal_subscriptions.getExplicitRecipientsRuleId()
+        if not hasattr(event, explicit_recipients_rule_id):
+            event.manage_addProduct[
+                'CPSSubscriptions'].addExplicitRecipientsRule()
+
+        explicit_recipients_rule = getattr(event, explicit_recipients_rule_id)
+        explicit_emails = REQUEST.form.get('explicit_emails', [])
+        explicit_recipients_rule.updateEmails(explicit_emails)
+
+
+        black_list = REQUEST.form.get('black_list', [])
+        event.updateRecipientEmailsBlackList(black_list)
+
+return REQUEST.RESPONSE.redirect("%s/%s?portal_status_message=%s" %(context.absolute_url(),
+                                                                    'folder_notifications_form',
+                                                                    'psm_notifications_changed'))
