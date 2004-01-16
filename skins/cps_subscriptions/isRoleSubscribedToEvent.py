62020fe69c16ded23984e875d299b7c58bc1b992
janguenot <devnull@localhost>
2004-01-16 11:48:56 +0000

    We need to be sure that the RecipientsRule is type RoleRecipientsRule

--- skins/cps_subscriptions/isRoleSubscribedToEvent.py
+++ skins/cps_subscriptions/isRoleSubscribedToEvent.py
@@ -8,6 +8,8 @@ portal_subscriptions = context.portal_subscriptions
 subscriptions = portal_subscriptions.getSubscriptionsFor(event_type, context, infos={})
 for subscription in subscriptions:
     subscription_recipients_rule = subscription.getRecipientsRules()
+    subscription_recipients_rule = [
+        x for x in subscription_recipients_rule if hasattr(x, 'getRoles')]
     for recipients_rule in subscription_recipients_rule:
         if role in recipients_rule.getRoles():
             return 1
