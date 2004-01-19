84751d88dcfca6ff233d13f91c99191c94454962
janguenot <devnull@localhost>
2004-01-19 10:46:36 +0000

    Refactoring

--- skins/cps_subscriptions/getAllRecipientsFor.py
+++ skins/cps_subscriptions/getAllRecipientsFor.py
@@ -7,11 +7,13 @@ only the recipients for this given event type. If not
 all the recipients for all the events are returned.
 """
 
+from zLOG import LOG, DEBUG
+
 portal_subscriptions = context.portal_subscriptions
 events = context.getEventTypesFromContext()
 recipients = {}
 for event in events.keys():
-    event_recipients = portal_subscriptions.getRecipientsFor(event, context)
-    for recipient_mail in event_recipients.keys():
-        recipients[recipient_mail] = event_recipients[recipient_mail]
+    recipients_mails = portal_subscriptions.getRecipientsFor(event, context)
+    for recipient_mail in recipients_mails.keys():
+        recipients[recipient_mail] = recipients_mails[recipient_mail]
 return recipients
