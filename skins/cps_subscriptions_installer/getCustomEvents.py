139886c84d2099a95d80b3518db854b912b1ef69
janguenot <devnull@localhost>
2004-01-19 23:42:34 +0000

    Installer scripts

---
+++ skins/cps_subscriptions_installer/getCustomEvents.py
@@ -0,0 +1,13 @@
+##parameters=
+#$Id$
+""" Returns the custom Subscriptions events
+
+Use by the installer at setup/update time.
+Check the getEvents syntax and override this file within your product.
+The dict key is the portal type of the container where
+you'd like to be able to get notifications features.
+*IMPORTANT* if you define a new dict key the default ones will be override.
+"""
+###################################
+
+return {}
---
+++ skins/cps_subscriptions_installer/getEvents.py
@@ -0,0 +1,29 @@
+##parameters=
+#$Id$
+""" Returns the CPS Subscriptions events
+
+Use by the installer.  The dict key is the portal type of the container where
+you'd like to be able to get notifications features.
+"""
+
+dict = {}
+
+workspace = {'workflow_create' : 'label_creation_of_documents',
+             'workflow_modify' : 'label_modification_of_documents',
+             'workflow_cut_copy_paste' : 'label_cut_copy_paste_of_documents',
+             }
+
+###################################
+
+dict['Workspace'] = workspace
+
+#
+# Custom events
+#
+
+custom = context.getCustomEvents()
+
+for key in custom.keys():
+    dict[key] = custom[key]
+
+return dict
