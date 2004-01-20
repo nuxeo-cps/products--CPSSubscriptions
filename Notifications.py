7b6f5b41636ca941ca97b03de60e72999b519003
janguenot <devnull@localhost>
2004-01-20 01:15:05 +0000

    API + cleaning + copyrights

--- Notifications.py
+++ Notifications.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2003 Nuxeo SARL <http://nuxeo.com>
-# Copyright (C) 2003 CGEY <http://cgey.com>
-# Copyright (c) 2003 Ministère de L'intérieur (MISILL)
+# Copyright (C) 2004 Nuxeo SARL <http://nuxeo.com>
+# Copyright (C) 2004 CGEY <http://cgey.com>
+# Copyright (c) 2004 Ministère de L'intérieur (MISILL)
 #               <http://www.interieur.gouv.fr/>
 # Authors : Julien Anguenot <ja@nuxeo.com>
 #           Florent Guillaume <fg@nuxeo.com>
@@ -32,14 +32,15 @@ subobjects, like RecipientsRule.
 
 """
 
-from Globals import InitializeClass, DTMLFile, MessageDialog
+from Globals import InitializeClass, MessageDialog
 from Acquisition import aq_base, aq_parent, aq_inner
 from AccessControl import ClassSecurityInfo
 
-from Products.CMFCore.PortalFolder import PortalFolder
+from Products.CMFCore.CMFCorePermissions import ManagePortal
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.PortalFolder import PortalFolder
 
-from zLOG import LOG, DEBUG, INFO
+from zLOG import LOG, INFO
 
 class NotificationRule(PortalFolder):
     """Base Notification rule Class.
@@ -48,10 +49,10 @@ class NotificationRule(PortalFolder):
     type.
     """
 
-    def notifyRecipients(self, emails=[], members=[], **kw):
+    def notifyRecipients(self, **kw):
         """ Notify recipients
 
-        This method will be called by the Subscription method.
+        This method will be called by the Subscription object.
         """
         raise NotImplementedError
 
@@ -60,21 +61,109 @@ InitializeClass(NotificationRule)
 ################################################################
 
 class MailNotificationRule(NotificationRule):
-    """Mail Notification
+    """Mail Notification Rule
 
     Sending mail to the recipients of the subscription.
+    Only one MailNotifcationRule is necessarly within a subscription.
     """
 
     meta_type = "Mail Notification Rule"
     portal_type = meta_type
 
-    def notifyRecipients(self, emails=[], members=[], **kw):
+    security = ClassSecurityInfo()
+
+    def _getMailFrom(self, object):
+        """ Return an email for the mail from field of the mail.
+
+        1 - Creator of the object if an object has been created.
+        2 - CPS Administator.
+        3 - Others ?
+        """
+
+        mtool = self.portal_membership
+        creator = object.Creator()
+        email_creator = mtool.getMemberById(creator).getProperty('email')
+
+        if email_creator:
+            return email_creator
+        else:
+            pprops = self.portal_properties
+            cps_admin_email = pprops.get('email_from_address',
+                                         'no_mail@nuxeo.com')
+            return cps_admin_email
+
+    def _getSubject(self, infos):
+        """ Returns the subject of the email.
+
+        Is proccessed with the infos given as parameters.
+        """
+        return self.getMailTemplate(infos=infos)['mail_subject']
+
+    def _getBody(self, infos):
+        """ Returns the body of the email.
+
+        Is proccessed with the infos given as parameters.
+        """
+        return self.getMailTemplate(infos=infos)['mail_body']
+
+    def _makeInfoDict(self, event_type, object):
+        """Building the infos dict used for processing the email.
+        """
+
+        infos = {}
+
+        portal_subscriptions = getToolByName(self, 'portal_subscriptions')
+        mcat = self.Localizer.default
+
+        events_from_context = portal_subscriptions.getEventsFromContext(
+            context=aq_parent(aq_inner(object)))
+
+        event_from_context = mcat(events_from_context[event_type]).encode(
+            "ISO-8859-15", 'ignore')
+
+        infos['notification_title'] = event_from_context
+        infos['event'] = event_type
+
+        infos['object_title'] = object.Title()
+        infos['object_url'] = object.absolute_url()
+        infos['object_type'] = getattr(object, 'portal_type', '')
+
+        infos['user_id'] = object.Creator()
+        infos['user_name'] = getattr(self.portal_membership.getMemberById(
+            object.Creator()), 'fullname', '')
+        return infos
+
+    security.declareProtected(ManagePortal, "notifyRecipients")
+    def notifyRecipients(self, event_type, object,
+                         infos=None, emails=[], members=[], groups=[], **kw):
         """ Notify recipients
 
-        This method will be called by the Subscription method.
+        This method will be called by the Subscription object.
         """
-        raise NotImplementedError
 
+        #
+        # Dealing only with emails right now.
+        # to implement : members and groups
+        #
+
+        infos = self._makeInfoDict(event_type, object)
+
+        LOG(":: CPSSubscriptions :: MailNotificationRule :: on",
+            INFO,
+            infos)
+
+        mfrom = self._getMailFrom(object)
+        subject = self._getSubject(infos)
+        body = self._getBody(infos)
+
+        for email in emails:
+            LOG("::MailNotificationRule :: SENDING MAIL :: TO ",
+                INFO,
+                email)
+            self.MailHost.send(messageText=body,
+                               mto=[email],
+                               mfrom=mfrom,
+                               subject=subject,)
 
 InitializeClass(MailNotificationRule)
 
@@ -83,6 +172,7 @@ def addMailNotificationRule(self, id=None, title='', REQUEST=None, **kw):
     """
 
     id = self.portal_subscriptions.getMailNotificationRuleObjectId()
+    title = 'NOTIFICATION RULE'
 
     if hasattr(aq_base(self), id):
         return MessageDialog(
@@ -93,8 +183,5 @@ def addMailNotificationRule(self, id=None, title='', REQUEST=None, **kw):
     ob = MailNotificationRule(id, title=title, **kw)
     self._setObject(id, ob)
 
-    LOG('addRoleRecipientsRule', INFO,
-        'adding recipients rule  %s/%s' % (self.absolute_url(), id))
-
     if REQUEST is not None:
-        REQUEST['RESPONSE'].redirect(self.absolute_url()+'/manage_main')
+        REQUEST.RESPONSE.redirect(self.absolute_url()+'/manage_main')
