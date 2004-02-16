8d8e885f439ba25eab26005b0580885cc7a8bf83
janguenot <devnull@localhost>
2004-02-16 16:08:44 +0000

    Used now at subscriptions tool creation

--- skins/cps_subscriptions/getMailTemplate.py
+++ skins/cps_subscriptions/getMailTemplate.py
@@ -1,11 +1,11 @@
-##parameters=infos
+##parameters=
 #$Id$
 """ Mail Template
 
 XXX do sthg else than that -> body rendered with DTML
 """
 
-mail_subject = '[Notification] %(notification_title)s for object %(object_title)s' %infos
+mail_subject = '[Notification] %(notification_title)s for object %(object_title)s'
 mail_body = \
 """%(object_type)s has created a %(notification_title)s notification.
 
@@ -13,10 +13,17 @@ Additional info:
 User name: %(user_name)s (%(user_id)s)
 Event type: %(event)s
 URL: %(object_url)s
-""" %infos
+"""
+
+mail_error_body = \
+"""An error occurs while computing the body of the email.
+
+Please contact, the administrator of the portal.
+"""
 
 template = {}
 template['mail_subject'] = mail_subject
 template['mail_body'] = mail_body
+template['mail_error_body'] = mail_error_body
 
 return template
