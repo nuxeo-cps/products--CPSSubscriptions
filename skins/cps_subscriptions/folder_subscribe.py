dbd88edf663da5b46b72398d00a01f1dd3a793fb
janguenot <devnull@localhost>
2004-03-03 17:00:03 +0000

    More

--- skins/cps_subscriptions/folder_subscribe.py
+++ skins/cps_subscriptions/folder_subscribe.py
@@ -9,40 +9,22 @@ from zLOG import LOG, DEBUG
 
 if REQUEST is not None:
     if REQUEST.form:
-
         # @www
         email = REQUEST.form.get('subscriber_email', None)
         event_ids = REQUEST.form.get('event_ids', None)
-
-
-        # Checking the requested events
+        subscriptions_tool = context.portal_subscriptions
+        subscription_folder = subscriptions_tool.getSubscriptionContainerFromContext(context)
         for event_id in event_ids:
-            if not hasattr(subscription_folder, event_id):
-                subscription_folder.manage_addProduct[
-                    'CPSSubscriptions'].addSubscription(id=event_id)
-
-        #
-        # Checking the explicit recipients rules
-        #
-
-        for event_id in event_ids:
-            event = getattr(subscription_folder, event_id)
+            internal_event_id = 'subscription__'+event_id
+            event = getattr(subscription_folder, internal_event_id)
             explicit_recipients_rule_id = context.portal_subscriptions.getExplicitRecipientsRuleId()
-            if not hasattr(event, explicit_recipients_rule_id):
-                event.manage_addProduct[
-                    'CPSSubscriptions'].addExplicitRecipientsRule()
-
-            # Default -> mail notification
-            if not hasattr(event, context.portal_subscriptions.getMailNotificationRuleObjectId()):
-                event.manage_addProduct[
-                    'CPSSubscriptions'].addMailNotificationRule()
-
             explicit_recipients_rule = getattr(event, explicit_recipients_rule_id)
-            if explicit_recipients_rule.updatePendingEmails(email):
-                psm = 'psm_subscription_taken_into_consideration'
+            if explicit_recipients_rule.anonymousSubscribe(email, event_id, context):
+                psm = 'psm_email_sent_to_you'
             else:
                 psm = 'psm_subscription_not_taken_into_consideration'
 
-return REQUEST.RESPONSE.redirect("%s/%s?portal_status_message=%s" %(context.absolute_url(),
-                                                                    'folder_subscribe_form',
-                                                                    psm))
+return REQUEST.RESPONSE.redirect("%s/%s?portal_status_message=%s&email=%s" %(context.absolute_url(),
+                                                                             'folder_subscribe_form',
+                                                                             psm,
+                                                                             email))
