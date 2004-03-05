f6fca8b2ea3cd243124d784d45ddf7c9fb493356
janguenot <devnull@localhost>
2004-03-05 13:14:39 +0000

    Local roles in sections

--- skins/cps_subscriptions/getLocalRolesFromContext.py
+++ skins/cps_subscriptions/getLocalRolesFromContext.py
@@ -8,14 +8,14 @@ workspaces_local_roles = {'WorkspaceManager': 'label_workspace_manager',
                           'WorkspaceMember' : 'label_workspace_member',
                           'WorkspaceReader' : 'label_workspace_reader',
                           }
-#sections_local_roles = {'SectionManager'  : 'label_section_manager',
-#                        'SectionReviewer' : 'label_section_reviewer',
-#                        'SectionReader'   : 'label_section_reader',
-#                        }
+sections_local_roles = {'SectionManager'  : 'label_section_manager',
+                        'SectionReviewer' : 'label_section_reviewer',
+                        'SectionReader'   : 'label_section_reader',
+                        }
 
 if context.portal_type == 'Workspace':
     return workspaces_local_roles
 elif context.portal_type == 'Section':
-    raise('Subscriptions not implemented for : %s' %(context.portal_type))
+    return sections_local_roles
 else:
     raise('Subscriptions not implemented for : %s' %(context.portal_type))
