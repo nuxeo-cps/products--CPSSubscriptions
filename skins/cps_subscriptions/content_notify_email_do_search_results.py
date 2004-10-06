433dab23208dfed9564422b7b721db8708b6eb14
janguenot <devnull@localhost>
2004-10-06 14:17:30 +0000

    Notify content : use the members directory layouts for searching people + form

---
+++ skins/cps_subscriptions/content_notify_email_do_search_results.py
@@ -0,0 +1,50 @@
+##parameters=dir, datastructure, **kw
+
+from cgi import escape
+
+datamodel = datastructure.getDataModel()
+
+mapping = {}
+for key, value in datamodel.items():
+    if value:
+        mapping[key] = value
+
+result_fields = context.getDirectoryResultFields(dir.getId(),
+                                                 dir.title_field)
+
+return_fields = []
+sort_by = None
+sort_direction = None
+process_fields = {}
+for field in result_fields:
+    return_fields.append(field['id'])
+    sorted = field.get('sort')
+    if sorted == 'asc':
+        sort_by = field['id']
+        sort_direction = 'asc'
+    elif sorted == 'desc':
+        sort_by = field['id']
+        sort_direction = 'desc'
+    if field.get('process'):
+        process_fields[field['id']] = field['process']
+
+results = dir.searchEntries(return_fields=return_fields, **mapping)
+
+for field, process_meth in process_fields.items():
+    meth = getattr(context, process_meth, None)
+    if not meth:
+        continue
+    for item in results:
+        value = item[1].get(field)
+        item[1][field] = meth(field, value)
+
+if sort_by:
+    items = [(item[1].get(sort_by), item) for item in results]
+    items.sort()
+    if sort_direction == 'desc':
+        items.reverse()
+    results = [item[1] for item in items]
+
+rendered = dir.content_notify_email_search_results(results=results)
+
+return rendered, 'results'
---
+++ skins/cps_subscriptions/content_notify_sendmail.py
@@ -0,0 +1,46 @@
+##parameters=REQUEST=None, **kw
+
+
+if REQUEST is not None:
+    form = REQUEST.form
+    members = form.get('members', [])
+    explicit_recipients_emails = form.get('explicit_recipients_emails', [])
+    mail_subject = form.get('mail_subject', '')
+
+    #
+    # Add a link to the current content at the end of the mail_body
+    #
+
+    mcat = context.Localizer.default
+
+    mail_body = form.get('mail_body', '')
+    mail_body = mail_body + \
+                '\n' + \
+                mcat('label_notification_related_document').encode('ISO-8859-15', 'ignore') + \
+                ' : ' + \
+                '<a href="%s"' %context.absolute_url() + \
+                '>%s'%context.title_or_id() + \
+                '</a>'
+
+    mtool = context.portal_membership
+    member = mtool.getAuthenticatedMember()
+    sender_email = member.getProperty('email')
+
+    tos = explicit_recipients_emails
+    for member_id in members:
+        tos.append(context.getMemberEmail(member_id))
+
+    to_str = ''
+    for to in tos:
+        to_str += to + ','
+
+    infos = {'body'    : (mail_body, 'text/plain'),
+             'subject' : mail_subject,
+             'sender_email' : sender_email,
+             'to'           : to_str[:len(to_str)-1]}
+
+    subtool = context.portal_subscriptions
+    cerror = subtool.sendmail(infos=infos)
+    psm = 'psm_an_email_has_been_sent'
+    redirect_url = REQUEST['URL1'] + '?portal_status_message=%s' %psm
+    return REQUEST.RESPONSE.redirect(redirect_url)
