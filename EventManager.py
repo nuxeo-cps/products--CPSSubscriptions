ee66e6fd25650eb5d0227c3b16067614ec16f7cb
janguenot <devnull@localhost>
2005-08-02 19:12:34 +0000

    Add a first version of the EventManager with couple of tests. Needs to be plugged into subscriptions tool

---
+++ EventManager.py
@@ -0,0 +1,123 @@
+# -*- coding: iso-8859-15 -*-
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Authors: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""Manager for event that can be that can be delayed until commit
+time.
+
+Asynchronous by default.
+"""
+
+from zLOG import LOG, DEBUG
+from Acquisition import aq_base
+
+from Products.CMFCore.utils import getToolByName
+
+try:
+    import transaction
+except ImportError:
+    # BBB: for Zope 2.7
+    from Products.CMFCore.utils import transaction
+    # The following is missing from CMF 1.5.2
+    def BBBget():
+        return get_transaction()
+    transaction.get = BBBget
+
+
+_TXN_MGR_ATTRIBUTE = '_cps_event_manager'
+
+class EventManager:
+    """Holds events that need to be processed."""
+
+    # Not synchronous by default
+    # XXX This may be monkey-patched by unit-tests.
+    DEFAULT_SYNC = False
+
+    def __init__(self, txn):
+        """Initialize and register this manager with the transaction."""
+        self._events = {}
+        self._sync = self.DEFAULT_SYNC
+        txn.beforeCommitHook(self)
+
+    def setSynchonous(self, sync):
+        """Set queuing mode."""
+        if sync:
+            self()
+        self._sync = sync
+
+    def isSynchonous(self):
+        """Get queuing mode."""
+        return self._sync
+
+    def push(self, event_type, object, info):
+        """Push the event in a queue
+        """
+        rpath = '/'.join(object.getPhysicalPath())[1:]
+
+        i = (id(aq_base(object)), rpath)
+        eid = (event_type, i)
+
+        event_info = {'id' : i,
+                      'object': object,
+                      }
+
+        cinfo = self._events.get(eid)
+        if cinfo is None:
+            self._events[eid] = (event_info, info)
+        else:
+            # XXX probably not what's needed here.
+            self._events[eid][0].update(event_info)
+            self._events[eid][1].update(info)
+
+    def __call__(self):
+        """Called when transaction commits.
+
+        Dispatch the events to the susbcriptions tool todo the actual
+        processing
+        """
+        for k, v in self._events.items():
+            ob = v[0]['object']
+            root = ob.getPhysicalRoot()
+            path = ob.getPhysicalPath()
+            old_ob = ob
+            ob = root.unrestrictedTraverse(path, None)
+            if ob is None:
+                LOG("EventManager", DEBUG, "Object %r disappeard"%old_ob)
+            else:
+                subtool = getToolByName(ob, 'portal_subscriptions')
+                if subtool is not None:
+                    LOG("EventManager", DEBUG, "Processing event for %r "%(ob))
+                    subtool.notify_processed_event(k[0], ob, v[1])
+                else:
+                    LOG("EventManager", DEBUG, "Subscriptions Tool not found")
+
+def _remove_event_manager():
+    txn = transaction.get()
+    setattr(txn, _TXN_MGR_ATTRIBUTE, None)
+
+def get_event_manager():
+    """Get the event manager.
+
+    Creates it if needed.
+    """
+    txn = transaction.get()
+    mgr = getattr(txn, _TXN_MGR_ATTRIBUTE, None)
+    if mgr is None:
+        mgr = EventManager(txn)
+        setattr(txn, _TXN_MGR_ATTRIBUTE, mgr)
+    return mgr
---
+++ tests/test_event_manager.py
@@ -0,0 +1,233 @@
+# -*- coding: iso-8859-15 -*-
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""Tests for the Indexation Manager
+"""
+
+import random
+import unittest
+from OFS.SimpleItem import SimpleItem
+
+from Products.CPSSubscriptions.EventManager import EventManager
+from Products.CPSSubscriptions.EventManager import get_event_manager
+
+try:
+    import transaction
+except ImportError: # BBB: for Zope 2.7
+    from Products.CMFCore.utils import transaction
+
+class DummySubscriptionsTool:
+
+    def __init__(self):
+        self.id = 'portal_subscriptions'
+        self.log = []
+
+    def getLog(self):
+        log = self.log
+        self.log = []
+        return log
+
+    def notify_processed_event(self, event_type, object, infos):
+        self.log.append("event_type : %s, object : %s , infos : %s"
+                        %(event_type, str(object.id), str(infos)))
+
+portal_subscriptions = DummySubscriptionsTool()
+
+class FakeTransaction:
+    def beforeCommitHook(self, hook):
+        pass
+
+class FakeRoot:
+
+    __objects__ = {}
+
+    def generateId(self):
+        id = random.randrange(1000000)
+        while id in self.__objects__.keys():
+            id = random.randrange(1000000)
+        return id
+
+    def unrestrictedTraverse(self, path, default):
+        dummy, id = path
+        assert dummy == ''
+        return self.getDummy(int(id))
+
+    def addDummy(self, cls=None):
+        id = self.generateId()
+        if cls is None:
+            cls = Dummy
+        ob = cls(id)
+        self.__objects__[id] = ob
+        return ob
+
+    def getDummy(self, id):
+        return self.__objects__.get(id)
+
+    def clear(self):
+        self.__objects__ = {}
+
+root = FakeRoot()
+
+class Dummy:
+
+    def __init__(self, id):
+        self.id = id
+        self.log = []
+        self.portal_subscriptions = portal_subscriptions
+
+    def getLog(self):
+        # get and clear log
+        log = self.log
+        self.log = []
+        return log
+
+    def getPhysicalRoot(self):
+        return root
+
+    def getPhysicalPath(self):
+        return ('', str(self.id))
+
+class EventManagerTest(unittest.TestCase):
+
+    def get_manager(self):
+        return EventManager(FakeTransaction())
+
+    def test_push_events(self):
+
+        mgr = self.get_manager()
+
+        # Push one
+        dummy1 = Dummy('dummy1')
+        mgr.push('event_id', dummy1, {})
+        self.assertEqual(len(mgr._events.keys()), 1)
+
+        # Push another one
+        dummy2 = Dummy('dummy2')
+        mgr.push('event_id', dummy2, {})
+        self.assertEqual(len(mgr._events.keys()), 2)
+
+        # Push the first one again with the same event
+        mgr.push('event_id', dummy1, {})
+        self.assertEqual(len(mgr._events.keys()), 2)
+
+        # Push the seconf one again with the same event
+        mgr.push('event_id', dummy2, {})
+        self.assertEqual(len(mgr._events.keys()), 2)
+
+        # Push the first one with another event
+        mgr.push('other_event_id', dummy1, {})
+        self.assertEqual(len(mgr._events.keys()), 3)
+
+        # Push the second one with another event
+        mgr.push('other_event_id', dummy2, {})
+        self.assertEqual(len(mgr._events.keys()), 4)
+
+    def test_manager_call(self):
+
+        mgr = self.get_manager()
+
+        dummy = root.addDummy()
+        mgr.push('event_id', dummy, {})
+
+        mgr()
+        self.assert_(
+            dummy.portal_subscriptions.getLog(),
+            ['event_type : event_id, object : %s , infos : {}' %dummy.id
+             ])
+
+        root.clear()
+
+    def test_synchronous(self):
+        mgr = self.get_manager()
+    
+        dummy = root.addDummy()
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+    
+        mgr.push('event_id', dummy, {})
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+    
+        mgr.setSynchonous(True)
+        self.assertEquals(
+            dummy.portal_subscriptions.getLog(),
+            ["event_type : event_id, object : %s , infos : {}" %dummy.id])
+    
+        mgr.setSynchonous(False)
+        mgr.push('event_id', dummy, {'c':'c'})
+        self.assertEquals(dummy.getLog(), [])
+    
+        mgr()
+        self.assertEquals(
+            dummy.portal_subscriptions.getLog(),
+            ["event_type : event_id, object : %s , infos : {'c': 'c'}"%dummy.id])
+        root.clear()
+
+class TransactionEventManagerTest(unittest.TestCase):
+
+    # These really test the beforeCommitHook
+
+    def test_transaction(self):
+        transaction.begin()
+        mgr = get_event_manager()
+        dummy = root.addDummy()
+        mgr.push('event_id', dummy, {})
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+        transaction.commit()
+        self.assertEquals(
+            dummy.portal_subscriptions.getLog(),
+            ["event_type : event_id, object : %s , infos : {}" %dummy.id])
+        root.clear()
+
+    def test_transaction_aborting(self):
+        transaction.begin()
+        mgr = get_event_manager()
+        dummy = root.addDummy()
+        mgr.push('event_id', dummy, {})
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+        transaction.abort()
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+        root.clear()
+        
+    
+    def test_transaction_nested(self):
+        transaction.begin()
+        mgr = get_event_manager()
+        dummy = root.addDummy()
+        other = root.addDummy()
+        dummy.other = other
+
+        mgr.push('event_id', dummy, {})
+        mgr.push('event_id', other, {})
+        
+        self.assertEquals(dummy.portal_subscriptions.getLog(), [])
+        transaction.commit()
+        self.assertEquals(
+            dummy.portal_subscriptions.getLog(),
+            ["event_type : event_id, object : %s , infos : {}"%other.id,
+             "event_type : event_id, object : %s , infos : {}"%dummy.id]
+            )
+        root.clear()
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(EventManagerTest),
+        unittest.makeSuite(TransactionEventManagerTest),
+        ))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(test_suite())
