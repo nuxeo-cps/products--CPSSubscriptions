80144e6a9ff40350cdbc95efab29817dc0a18834
janguenot <devnull@localhost>
2005-11-08 23:13:28 +0000

    Refactore this using the CPSSubscriptionsTestCase so that we can start adding more tests

--- tests/testSubscriptionsTool.py
+++ tests/testSubscriptionsTool.py
@@ -1,103 +1,80 @@
+# -*- coding: ISO-8859-15 -*-
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Julien Anguenot <anguenot@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
 # $Id$
 
-#
-# TODO :
-# - ZMI
-# - Misc API
-#
+import os
+import sys
 
-import os, sys
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py'))
-
-from types import StringType, ListType, DictType
 import unittest
+import CPSSubscriptionsTestCase
 
-from Products.CPSSubscriptions.SubscriptionsTool import \
-     SUBSCRIPTION_CONTAINER_ID, EXPLICIT_RECIPIENTS_RULE_ID,\
-     MAIL_NOTIFICATION_RULE_ID
+from Products.CPSSubscriptions import SubscriptionsTool
 
-from Products.CPSSubscriptions.SubscriptionsTool import SubscriptionsTool
-
-class Context:
-    def __init__(self):
-        self.portal_type = ''
-
-class TestSubscriptionsTool(unittest.TestCase):
+class TestSubscriptionsTool(
+    CPSSubscriptionsTestCase.CPSSubscriptionsTestCase):
     """Test Subscriptions Tool
-
-    This class tests the subscriptions tool :
-     - Fixtures
-     - Default settings
-     - Initialization
-     - ZMI
-     - Misc API
     """
 
-    subscriptions_tool = SubscriptionsTool()
-
-    def beforeTearDown(self):
-        # XXX
-        pass
+    def afterSetUp(self):
+        self.login('manager')
+        self._stool = self.portal.portal_subscriptions
 
     def testSubscriptionsToolFixtures(self):
-        #
-        # Test subscriptions tool fixtures
-        #
-        self.assertNotEqual(self.subscriptions_tool, None)
-        self.assertEqual(self.subscriptions_tool.id,
-                         'portal_subscriptions')
-        self.assertEqual(self.subscriptions_tool.meta_type,
-                         'Subscriptions Tool')
+        self.assertNotEqual(self._stool, None)
+        self.assertEqual(
+            self._stool.id, 'portal_subscriptions')
+        self.assertEqual(
+            self._stool.meta_type, 'Subscriptions Tool')
 
     def testSubscriptionsToolGlobalIds(self):
-        #
-        # Test Subscriptions tool global ids
-        #
-        stool = self.subscriptions_tool
-        self.assertEqual(stool.getSubscriptionContainerId(),
-                         SUBSCRIPTION_CONTAINER_ID)
-        self.assertEqual(stool.getExplicitRecipientsRuleId() ,
-                         EXPLICIT_RECIPIENTS_RULE_ID)
-        self.assertEqual(stool.getMailNotificationRuleObjectId(),
-                         MAIL_NOTIFICATION_RULE_ID)
-
-    ##def testSubscriptionsToolDefaultMessageElements(self):
-    ##    #
-    ##    # Test Subscriptions tool default message elements
-    ##    #
-    ##    default_message_title = self.subscriptions_tool.getDefaultMessageTitle()
-    ##    self.assertNotEqual(default_message_title, None)
-    ##    self.assertNotEqual(default_message_title, '')
-    ##    self.assert_(isinstance(default_message_title, StringType))
-    ##
-    ##    default_message_body = self.subscriptions_tool.getDefaultMessageBody()
-    ##    self.assertNotEqual(default_message_body, None)
-    ##    self.assertNotEqual(default_message_body, '')
-    ##    self.assert_(isinstance(default_message_body, StringType))
-    ##
-    ##    error_message_body = self.subscriptions_tool.getErrorMessageBody()
-    ##    self.assertNotEqual(error_message_body, None)
-    ##    self.assertNotEqual(error_message_body, '')
-    ##    self.assert_(isinstance(error_message_body, StringType))
+        self.assertEqual(
+            self._stool.getSubscriptionContainerId(),
+            SubscriptionsTool.SUBSCRIPTION_CONTAINER_ID)
+        self.assertEqual(
+            self._stool.getExplicitRecipientsRuleId() ,
+            SubscriptionsTool.EXPLICIT_RECIPIENTS_RULE_ID)
+        self.assertEqual(
+            self._stool.getMailNotificationRuleObjectId(),
+            SubscriptionsTool.MAIL_NOTIFICATION_RULE_ID)
 
     def testSubscriptionsToolEventsRegistration(self):
+
         #
         # Test events registration in all registred context
         #
 
+        class Context:
+            def __init__(self):
+                self.portal_type = ''
+
         context = Context()
-        portal_types = self.subscriptions_tool.getContainerPortalTypes()
-        self.assert_(isinstance(portal_types, ListType))
+        portal_types = self._stool.getContainerPortalTypes()
+        self.assert_(isinstance(portal_types, list))
 
         for portal_type in portal_types:
             context.portal_type = portal_type
-            events_in_context = self.subscriptions_tool.getEventsFromContext(
-                context)
+            events_in_context = self._stool.getEventsFromContext(context)
             self.assertNotEqual(events_in_context, {})
-            self.assert_(isinstance(events_in_context, DictType))
+            self.assert_(isinstance(events_in_context, dict))
 
     def testSubscriptionsToolRenderedPortalTypeRegistration(self):
+
         #
         # Tests adding some portal_types that have to be rendered at
         # notification time and then added to the notification email body
@@ -105,27 +82,23 @@ class TestSubscriptionsTool(unittest.TestCase):
 
         portal_type_ok = 'XXXX'
         portal_type_not_ok = ('XXXXXXXXXX',)
-        currents = self.subscriptions_tool.getRenderedPortalTypes()
+        currents = self._stool.getRenderedPortalTypes()
         initial_len = len(currents)
 
-        self.assert_(isinstance(currents, ListType))
+        self.assert_(isinstance(currents, list))
 
-        self.assertEqual(self.subscriptions_tool.addRenderedPortalType(
-            portal_type_ok),
-                         1)
-        new_currents = self.subscriptions_tool.getRenderedPortalTypes()
+        self.assertEqual(self._stool.addRenderedPortalType(portal_type_ok), 1)
+        new_currents = self._stool.getRenderedPortalTypes()
         new_len = len(new_currents)
-
         self.assertEqual(initial_len+1, new_len)
+        self.assertEqual(
+            self._stool.addRenderedPortalType(portal_type_not_ok), 0)
 
-        self.assertEqual(self.subscriptions_tool.addRenderedPortalType(
-            portal_type_not_ok),
-                         0)
-
-        currents = self.subscriptions_tool.getRenderedPortalTypes()
+        currents = self._stool.getRenderedPortalTypes()
         self.assertEqual(len(currents), new_len)
 
     def testSubscriptionsToolRenderedEventsRegistration(self):
+
         #
         # Tests adding some events that have to be rendered at
         # notification time and then added to the notification email body
@@ -133,29 +106,30 @@ class TestSubscriptionsTool(unittest.TestCase):
 
         event_id_ok = 'XXXX'
         event_id_not_ok = ('XXXXXXXXXX',)
-        currents = self.subscriptions_tool.getRenderedEvents()
+        currents = self._stool.getRenderedEvents()
         initial_len = len(currents)
 
         # This variables could be initialized
         # Just check in here if the structure hosting is a list
-        self.assert_(isinstance(currents, ListType))
+        self.assert_(isinstance(currents, list))
 
         self.assertEqual(
-            self.subscriptions_tool.addRenderedEvent(event_id_ok),
-            1)
+            self._stool.addRenderedEvent(event_id_ok), 1)
 
-        currents_plus = self.subscriptions_tool.getRenderedEvents()
+        currents_plus = self._stool.getRenderedEvents()
         new_len = len(currents_plus)
 
         self.assertEqual(new_len, initial_len + 1)
 
         self.assertEqual(
-            self.subscriptions_tool.addRenderedEvent(event_id_not_ok),
-            0)
-        new_currents = self.subscriptions_tool.getRenderedEvents()
+            self._stool.addRenderedEvent(event_id_not_ok), 0)
+        new_currents = self._stool.getRenderedEvents()
         self.assertEqual(new_len, len(new_currents))
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestSubscriptionsTool))
     return suite
+
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
