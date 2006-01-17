e55a9c607d861eb333677b806660be3408cb685e
fguillaume <devnull@localhost>
2006-01-17 19:35:39 +0000

    Layer-based test.

--- tests/CPSSubscriptionsTestCase.py
+++ tests/CPSSubscriptionsTestCase.py
@@ -1,32 +1,12 @@
-from Testing import ZopeTestCase
-from Products.ExternalMethod.ExternalMethod import ExternalMethod
+from Products.CPSDefault.tests.CPSTestCase import CPSTestCase
+from Products.CPSDefault.tests.CPSDefaultLayer import ExtensionProfileLayerClass
 
-from Products.CPSDefault.tests import CPSTestCase
 
-ZopeTestCase.installProduct('CPSSubscriptions')
+class LayerClass(ExtensionProfileLayerClass):
+    extension_ids = ('CPSSubscriptions:default',)
 
-CPSSubscriptionsTestCase = CPSTestCase.CPSTestCase
+CPSSubscriptionsLayer = LayerClass(__name__, 'CPSSubscriptionsLayer')
 
-class CPSSubscriptionsInstaller(CPSTestCase.CPSInstaller):
-    def addPortal(self, id):
-        """Override the Default addPortal method installing
-        a Default CPS Site.
 
-        Will launch the external method for CPSSubscriptions too.
-        """
-
-        # CPS Default Site
-        CPSTestCase.CPSInstaller.addPortal(self, id)
-        portal = getattr(self.app, id)
-
-        # Install the CPSSubscriptions product
-        if 'cpssubscriptions_installer' not in portal.objectIds():
-            cpssubscriptions_installer = ExternalMethod('cpsubscriptions_installer',
-                                                        '',
-                                                        'CPSSubscriptions.install',
-                                                        'install')
-            portal._setObject('cpssubscriptions_installer', cpssubscriptions_installer)
-        portal.cpssubscriptions_installer()
-
-CPSTestCase.setupPortal(PortalInstaller=CPSSubscriptionsInstaller)
-
+class CPSSubscriptionsTestCase(CPSTestCase):
+    layer = CPSSubscriptionsLayer
