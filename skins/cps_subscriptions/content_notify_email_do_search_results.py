29cd119a82413499de6eb69df4ad1bc9023b476b
ogrisel <devnull@localhost>
2006-10-10 16:54:44 +0000

    notify search form did not take search criteria into consideration when local roles where affected to groups

--- skins/cps_subscriptions/content_notify_email_do_search_results.py
+++ skins/cps_subscriptions/content_notify_email_do_search_results.py
@@ -2,10 +2,10 @@
 
 datamodel = datastructure.getDataModel()
 
-mapping = {}
+original_mapping = {}
 for key, value in datamodel.items():
     if value:
-        mapping[key] = value
+        original_mapping[key] = value
 
 result_fields = context.getDirectoryResultFields(dir.getId(), dir.title_field)
 
@@ -28,11 +28,14 @@ for field in result_fields:
 
 call_context = kw.get('call_context')
 mtool = context.portal_membership
+
+users_mapping = original_mapping.copy()
+
 if call_context is not None:
     dict_roles = mtool.getMergedLocalRoles(call_context, withgroups=0)
     search_restricted_list = dict_roles.keys()
-    mapping[dir.id_field] = search_restricted_list
-results = dir.searchEntries(return_fields=return_fields, **mapping)
+    users_mapping[dir.id_field] = search_restricted_list
+results = dir.searchEntries(return_fields=return_fields, **users_mapping)
 
 #
 # Check if there's groups with local roles
@@ -43,17 +46,18 @@ groups = [x[len('group:'):] for x in dict_roles if x.startswith('group:')]
 
 # special handling of special groups to behave as the RecipientRules logic
 if "role:Authenticated" in groups and not aclu.is_role_authenticated_empty:
-    results = dir.searchEntries(return_fields=return_fields)
+    results = dir.searchEntries(return_fields=return_fields, **original_mapping)
     skip_groups = True
 elif "role:Anonymous" in groups and not aclu.is_role_anonymous_empty:
-    results = dir.searchEntries(return_fields=return_fields)
+    results = dir.searchEntries(return_fields=return_fields, **original_mapping)
     skip_groups = True
 else:
     skip_groups = False
 
 if not skip_groups:
     for group_id  in groups:
-        group_mapping = {'groups': [group_id]}
+        group_mapping = original_mapping.copy()
+        group_mapping['groups'] = [group_id]
         group_member_results = dir.searchEntries(return_fields=return_fields,
                                                  **group_mapping)
         for member in group_member_results:
