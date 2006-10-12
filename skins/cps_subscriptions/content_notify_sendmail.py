4f492f9d9ce5f0c26546c2e9ab54b129e3b81662
ogrisel <devnull@localhost>
2006-10-12 17:09:12 +0000

    use the reply to feature for the email notification form

--- skins/cps_subscriptions/content_notify_sendmail.py
+++ skins/cps_subscriptions/content_notify_sendmail.py
@@ -27,35 +27,32 @@ mail_body = mail_body + \
 
 mtool = getToolByName(context, 'portal_membership')
 member = mtool.getAuthenticatedMember()
-sender_email = mtool.getEmailFromUsername(member.getUserName())
-sender_name = member.getProperty('sn') + ' ' + member.getProperty('givenName')
+reply_to_email = mtool.getEmailFromUsername(member.getUserName())
+reply_to_name = member.getProperty('sn') + ' ' + member.getProperty('givenName')
 
 subtool = getToolByName(context, 'portal_subscriptions')
-def_semail, def_sname = subtool.getMailSenderInfo()
-
-if not sender_email:
-    sender_email = def_semail
-    sender_name = def_sname
+sender_email, sender_name = subtool.getMailSenderInfo()
 
 tos = explicit_recipients_emails
 for member_id in members:
     tos.append(mtool.getEmailFromUsername(member_id))
 
-to_str = ''
-for to in tos:
-    to_str += to + ','
+to_str = ','.join(tos)
 
 infos = {
-    'sender_email' : sender_email,
-    'sender_name'  : sender_name,
-    'body'         : (mail_body, 'text/plain'),
-    'subject'      : mail_subject,
-    'to'           : to_str[:len(to_str)-1],
+    'sender_email'   : sender_email,
+    'sender_name'    : sender_name,
+    'reply_to_email' : reply_to_email,
+    'reply_to_name'  : reply_to_name,
+    'body'           : (mail_body, 'text/plain'),
+    'subject'        : mail_subject,
+    'to'             : ','.join(tos),
     }
 
 cerror = subtool.sendmail(infos=infos)
 
 if REQUEST is not None:
     psm = 'psm_an_email_has_been_sent'
-    redirect_url = REQUEST['URL1'] + '?portal_status_message=%s' %psm
+    redirect_url = REQUEST['URL1'] + '?portal_status_message=%s' % psm
     return REQUEST.RESPONSE.redirect(redirect_url)
+
