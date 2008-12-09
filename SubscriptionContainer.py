481efa4d8efc1f25d74c90c7bd429ca53de35189
madarche <devnull@localhost>
2008-12-09 21:55:24 +0000

    Cosmit.

--- SubscriptionContainer.py
+++ SubscriptionContainer.py
@@ -1,24 +1,25 @@
-# -*- coding: ISO-8859-15 -*-
-# Copyright (C) 2004 Nuxeo SARL <http://nuxeo.com>
+# Copyright (C) 2004-2008 Nuxeo SAS <http://nuxeo.com>
 # Copyright (C) 2004 CGEY <http://cgey.com>
-# Copyright (c) 2004 Ministère de L'intérieur (MISILL)
+# Copyright (c) 2004 Ministere de L'interieur (MISILL)
 #               <http://www.interieur.gouv.fr/>
-# Authors : Julien Anguenot <ja@nuxeo.com>
-#           Florent Guillaume <fg@nuxeo.com>
-
+# Authors:
+# Julien Anguenot <ja@nuxeo.com>
+# Florent Guillaume <fg@nuxeo.com>
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
 # $Id$
 
 __author__ = "Julien Anguenot <mailto:ja@nuxeo.com>"
