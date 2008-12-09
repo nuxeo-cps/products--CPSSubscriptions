798d31771208d5c4eeedaadf22686a7bab8db3bd
madarche <devnull@localhost>
2008-12-09 14:28:03 +0000

    Whitespace-cleanup + cosmit.

--- SubscriptionsTool.py
+++ SubscriptionsTool.py
@@ -1,24 +1,25 @@
-# -*- coding: ISO-8859-15 -*-
-# Copyright (c) 2004 Nuxeo SARL <http://nuxeo.com>
+# Copyright (c) 2004-2008 Nuxeo SAS <http://nuxeo.com>
 # Copyright (c) 2004 CGEY <http://cgey.com>
-# Copyright (c) 2004 Ministère de L'intérieur (MISILL)
+# Copyright (c) 2004 Ministere de L'interieur (MISILL)
 #               <http://www.interieur.gouv.fr/>
-# Authors : Julien Anguenot <ja@nuxeo.com>
-#           Florent Guillaume <fg@nuxeo.com>
-
+# Authors:
+# Julien Anguenot <ja@nuxeo.com>
+# Florent Guillaume <fg@nuxeo.com>
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
 # $Id$
 
 __author__ = "Julien Anguenot <mailto:ja@nuxeo.com>"
@@ -96,7 +97,7 @@ class SubscriptionsTool(UniqueObject, CMFBTreeFolder, ActionProviderBase):
          'label' : 'Notify hidden files'}, # FIXME Unused for now
         {'id': 'user_is_sender',
          'type': 'boolean', 'mode':'w',
-         'label' : "Use user's email address as mail from"}, 
+         'label' : "Use user's email address as mail from"},
         {'id': 'max_recipients_per_notification',
          'type': 'int', 'mode':'w',
          'label': 'Max recipients per notification message'},
@@ -740,7 +741,7 @@ class SubscriptionsTool(UniqueObject, CMFBTreeFolder, ActionProviderBase):
             # a subscription container within.
             return self.getSubscriptionContainerFromContext(
                 aq_parent(aq_inner(context)), force_local_creation=True)
-        
+
         return getattr(context, subscription_id)
 
     security.declarePublic('getSusbcriptionContainerFromContext')
@@ -1094,7 +1095,7 @@ class SubscriptionsTool(UniqueObject, CMFBTreeFolder, ActionProviderBase):
         of the portal administrator name.
         """
         if self.user_is_sender:
-            member = getToolByName(self, 
+            member = getToolByName(self,
                                    'portal_membership').getAuthenticatedMember()
             address = member.getProperty('email', None)
             name = member.getProperty('fullname', None)
